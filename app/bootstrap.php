9ae3a28877172a4a6d900236d68a98b23b912763
bunramy <prokerj@gmail.com>
2020-04-07 14:57:12 +0700

    update

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,6 @@
-TEST
+<?php
+    // Load Library
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Controller.php';
+    require_once 'libraries/Database.php';
+?>
---
+++ app/controllers/Pages.php
@@ -0,0 +1,19 @@
+<?php
+    class Pages{
+        public function __construct() {
+
+        }
+
+        public function index(){
+
+        }
+
+        public function about($id){
+            echo 'about ' . $id;
+        }
+
+        public function edit(){
+            echo "Hello world";
+        }
+    }
+?>
